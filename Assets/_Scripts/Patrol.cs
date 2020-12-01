26b35a3b77261b6559d9a22d1ebae8540f4659cc
Krzysztof Tomaszek <krzysztoftomaszek413d@gmail.com>
2020-12-01 19:19:01 +0100

    Full Init

---
+++ Assets/_Scripts/Patrol.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+
+public class Patrol: MonoBehaviour
+{
+    public Transform[] pathPoints;
+    private int destinationPoint = 0;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GotoToPoint();
+    }
+
+    void GotoToPoint()
+    {        
+        if(pathPoints.Length == 0) return;
+        agent.destination = pathPoints[destinationPoint].position;
+        destinationPoint = (destinationPoint + 1) % pathPoints.Length;
+    }
+
+    void Update()
+    {
+        if(!agent.pathPending && agent.remainingDistance < 0.3f)
+            GotoToPoint();
+    }
+}
